fae14fa6f156a7fc9541edbcadf7b889d4acfba5
Michael Labieniec <michaellabieniec@gmail.com>
2013-06-25 16:53:44 -0400

    updated default

--- logio/recipes/default.rb
+++ logio/recipes/default.rb
@@ -1,11 +1,1 @@
-# per log.io documentation
-execute "npm config set unsafe-perm true" do
-    # update config only if it needs to be updated
-    not_if "npm config get unsafe-perm | grep true"
-end
-
-# install as global node package
-execute "npm install -g log.io --user 'ec2-user'" do
-    # install only if its not yet installed
-    not_if "npm list -g | grep log.io"
-end
+execute 'npm install -g log.io --user "ec2-user"'
