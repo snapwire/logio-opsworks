3982e90c07e698ffbb0a889f2912d8abd2d61a11
Michael Labieniec <michaellabieniec@gmail.com>
2013-06-25 17:05:21 -0400

    updated default

--- logio/recipes/default.rb
+++ logio/recipes/default.rb
@@ -1,13 +1,13 @@
 execute 'npm install -g log.io --user "ec2-user"'
 
-directory "/home/e2-user/.log.io" do
+directory "/home/ec2-user/.log.io" do
   action :create
   mode 0755
   owner 'ec2-user'
   group 'ec2-user'
 end
 
-template "/home/e2-user/.log.io/web_server.conf" do
+template "/home/ec2-user/.log.io/web_server.conf" do
     source "server.conf.erb"
     action :create
     mode "644"
