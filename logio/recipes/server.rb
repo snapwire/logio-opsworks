e403248a20acd531aa8b3fa8c1bafa0bfda918c5
Michael Labieniec <michaellabieniec@gmail.com>
2013-06-25 16:43:57 -0400

    updated default

--- logio/recipes/server.rb
+++ logio/recipes/server.rb
@@ -1,5 +1,12 @@
 # add iptables rule for the file in templates/default/port_logio.erb
 # iptables_rule "port_logio"
+directory "/home/e2-user/.log.io" do
+  action :create
+  mode 0755
+  owner 'ec2-user'
+  group 'ec2-user'
+end
+
 template "/home/e2-user/.log.io/web_server.conf" do
     source "server.conf.erb"
     action :create
