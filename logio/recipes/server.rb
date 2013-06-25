8437ea342fd6a8f1f4cb64939335494fcdfbab24
Michael Labieniec <michaellabieniec@gmail.com>
2013-06-25 15:36:10 -0400

    updated for opsworks

--- logio/recipes/server.rb
+++ logio/recipes/server.rb
@@ -1,20 +1,18 @@
 # add iptables rule for the file in templates/default/port_logio.erb
-iptables_rule "port_logio"
+# iptables_rule "port_logio"
 
 # stop command
-execute "#{node['nodejs']['dir']}/bin/log.io server stop"
+execute "log.io server stop"
 
 template "/etc/log.io/server.conf" do
     source "server.conf.erb"
     action :create
     mode "644"
     variables({
-        :port => node['logio']['server']['port'],
-        :user => node['logio']['server']['basic_auth']['username'],
-        :pass => node['logio']['server']['basic_auth']['password']
+        :port => 8998
     })
 end
 
 # start command
-execute "#{node['nodejs']['dir']}/bin/log.io server start"
+execute "log.io server start"
 
