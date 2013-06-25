e59bf57ff3698e6f76daf5346cac775304401ecc
Michael Labieniec <michaellabieniec@gmail.com>
2013-06-25 16:58:47 -0400

    updated default

--- logio/recipes/default.rb
+++ logio/recipes/default.rb
@@ -1,1 +1,15 @@
 execute 'npm install -g log.io --user "ec2-user"'
+
+template "/home/e2-user/.log.io/web_server.conf" do
+    source "server.conf.erb"
+    action :create
+    mode "644"
+    variables({
+        :port => 8998,
+        :user => '',
+        :pass => ''
+    })
+end
+
+# start command
+execute "log.io-server start"
